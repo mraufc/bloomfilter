dcadf1f57a4045e25c3cc11485ff94e850a2c4c6
mraufc <rauf@eftsoftware.com>
2017-10-05 22:39:58 +0300

    writing test cases

--- bloomfilter.go
+++ bloomfilter.go
@@ -20,230 +20,36 @@
 package main
 
 import (
+	"log"
 	"fmt"
-	"sync"
 	"math"
-	"strconv"
-	"math/rand"
+	"hash/fnv"
+	"sync"
 	// "time"
 )
 
-type HashFunction func([]byte) uint64
+// HashFunction64 is a function type that takes a byte slice as input and returns hash value of uint64 type
+type HashFunction64 func([]byte) uint64
 
+// BloomFilter is a space-efficient probabilistic data structure, conceived by Burton Howard Bloom in 1970,
+// that is used to test whether an element is a member of a set.
+// Bloom filter - Wikipedia
+// https://en.wikipedia.org/wiki/Bloom_filter
 type BloomFilter struct {
-	hash1 HashFunction
-	hash2 HashFunction
+	hash1            HashFunction64
+	hash2            HashFunction64
 	numHashFunctions uint8
-	size uint64				//in bits
-	bits []uint64
-	threadSafe bool
+	size             uint64 //in bits
+	bits             []uint64
 }
 
+// BloomFilterTS is a BloomFilter structure with a RWMutex for thread safety.
 type BloomFilterTS struct {
-	bf *BloomFilter
+	bf  *BloomFilter
 	mtx sync.RWMutex
 }
 
-func main() {
-	x := uint64(0)
-	fmt.Println(x)
-
-	x |= (1 << 0)
-	fmt.Println(x)
-
-	x |= (1 << 2)
-	fmt.Println(x)
-
-	x &^= (1 << 7)
-
-	fmt.Println(x)
-	x &^= (1 << 2)
-	
-		fmt.Println(x)
-		x &^= (1 << 0)
-		
-			fmt.Println(x)
-
-	aaa := strconv.FormatUint(math.MaxUint64, 2)
-	fmt.Println(aaa)
-
-	// bf := NewBySizeAndNumHashFuncs(0, 0, nil, nil)
-
-	bf := NewByEstimates(90000000, 0.01, nil, nil)
-	bf.Add([]byte("a"))
-	bf.Add([]byte("b"))
-	bf.Add([]byte("c"))
-	bf.Add([]byte("d"))
-	bf.Add([]byte("e"))
-	bf.Add([]byte("f"))
-	bf.Add([]byte("g"))
-
-	result := bf.Query([]byte("a"))
-	fmt.Println(result)
-
-	result = bf.Query([]byte("b"))
-	fmt.Println(result)
-	result = bf.Query([]byte("c"))
-	fmt.Println(result)
-	result = bf.Query([]byte("d"))
-	fmt.Println(result)
-	result = bf.Query([]byte("e"))
-	fmt.Println(result)
-	result = bf.Query([]byte("f"))
-	fmt.Println(result)
-	result = bf.Query([]byte("g"))
-	fmt.Println(result)
-	result = bf.Query([]byte("h"))
-	fmt.Println(result)
-	result = bf.Query([]byte("j"))
-	fmt.Println(result)
-
-	result = bf.Query([]byte("rauf"))
-	fmt.Println(result)
-
-	bf.Add([]byte("rauf"))
-
-	result = bf.Query([]byte("rauf"))
-	fmt.Println(result)
-
-	for x:= 0 ; x<90000000 ; x++{
-		str := RandStringBytes( 1000)
-		bf.Add([]byte(str))
-	}
-
-	for {}
-
-
-
-	// tix := time.Tick(1 * time.Second)
-	// for {
-	// 	select {
-	// 	case <-tix :
-	// 		result := bf.Query([]byte("rauf"))
-	// 		fmt.Println(result)
-	// 	default:
-	// 		time.Sleep(50 * time.Millisecond)
-	// 	}
-	// }
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-func RandStringBytes(n int) string {
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = letterBytes[rand.Intn(len(letterBytes))]
-    }
-    return string(b)
-}
-
-func NewTSByEstimates(numItems uint64, fpRate float64, hash1 HashFunction, hash2 HashFunction) *BloomFilterTS {
-	size := uint64(math.Ceil(-1 * float64(numItems) * math.Log(fpRate) / math.Pow(math.Log(2), 2)))
-	numHashFunctions := uint8(math.Ceil(math.Log(2) * float64(size) / float64(numItems)))
-	fmt.Printf("size %v, numHashFunctions %v", size, numHashFunctions)
-	
-	bf := NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
-
-	return &BloomFilterTS{bf : bf}
-} 
-
-func NewTSBySizeAndNumHashFuncs(size uint64, numHashFunctions uint8, hash1 HashFunction, hash2 HashFunction) *BloomFilterTS {
-	bf := NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
-
-	return &BloomFilterTS{bf : bf}
-}
-
-func NewByEstimates(numItems uint64, fpRate float64, hash1 HashFunction, hash2 HashFunction) *BloomFilter {
-	size := uint64(math.Ceil(-1 * float64(numItems) * math.Log(fpRate) / math.Pow(math.Log(2), 2)))
-	numHashFunctions := uint8(math.Ceil(math.Log(2) * float64(size) / float64(numItems)))
-	fmt.Printf("size %v, numHashFunctions %v", size, numHashFunctions)
-	return NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
-}
-
-func NewBySizeAndNumHashFuncs(size uint64, numHashFunctions uint8, hash1 HashFunction, hash2 HashFunction) *BloomFilter {
-		if hash1 == nil {
-			hash1 = func(input []byte) uint64 {
-				retVal := uint64(0)
-				for i := 0; i < len(input); i++ {
-					retVal = retVal * 101 + uint64(input[i])
-				}
-				return retVal
-			}
-		}
-
-		if hash2 == nil {
-			hash2 = func(input []byte) uint64 {
-				retVal := uint64(0)
-				for i := 0; i < len(input); i++ {
-					retVal = retVal * 31 + uint64(input[i])
-				}
-				return retVal
-			}
-		}
-		
-		if size == 0 {
-			size = uint64(8 * 1024 * 1024 * 100) // 10 mb in bits
-		}
-
-		l := (size - (size % 64)) / 64
-
-		if size % 64 > 0 {
-			l++
-		}
-
-		fmt.Printf("size -> %v l -> %v\n", size, l)
-
-		bits := make([]uint64, l, l)
-
-		fmt.Printf("bits len is %v, cap is %v, memory should be %v bytes, %v kbytes, %v mbytes \n", len(bits), cap(bits), len(bits) * 8, len(bits) * 8 / 1024, len(bits) * 8 / 1024 / 1024)
-
-		if numHashFunctions == uint8(0) {
-			numHashFunctions = uint8(11)
-		}
-		
-		bf := BloomFilter{
-			hash1 : hash1, 
-			hash2 : hash2,
-			numHashFunctions : numHashFunctions,
-			size : size,
-			bits : bits,
-		}
-
-		return &bf
-}
-
-
-
-func (bf *BloomFilter) getBitLocations(data []byte) []uint64 {
-	hash1Val := bf.hash1(data)
-	hash2Val := bf.hash2(data)
-
-	retVal := make([]uint64, bf.numHashFunctions)
-	
-	for i := uint8(0); i < bf.numHashFunctions; i++ {
-		retVal[i] = (hash1Val + uint64(i) * hash2Val) % (bf.size / 64)
-	}
-	// fmt.Print(string(data))
-	// fmt.Print(" ")
-	// fmt.Print(retVal)
-	// fmt.Println("")
-	
-	return retVal
-}
-
-func (bfts *BloomFilterTS) Add(data []byte) {
-	bfts.mtx.Lock()
-	bfts.bf.Add(data)
-	bfts.mtx.Unlock()
-}
-
-func (bfts *BloomFilterTS) Query(data []byte) bool {
-	bfts.mtx.RLock()
-	defer bfts.mtx.RUnlock()
-	retVal := bfts.bf.Query(data)
-	return retVal
-}
-
+// Add takes a byte slice as input and adds it to the BloomFilter structure's bit array.
 func (bf *BloomFilter) Add(data []byte) {
 	bitLocations := bf.getBitLocations(data)
 
@@ -254,16 +60,168 @@ func (bf *BloomFilter) Add(data []byte) {
 	}
 }
 
+func main() {
+	bf := NewByEstimates(uint64(10), 0.01, nil, nil)
+	bf.Add([]byte("asdf"))
+	
+	fmt.Println(bf.Query([]byte("asdf")))
+
+	bf.Add([]byte("aahahahah"))
+	
+	fmt.Println(bf.Query([]byte("aahahahah")))
+
+	bf.Add([]byte("qqqqqqqqqq"))
+	
+	fmt.Println(bf.Query([]byte("qqqqqqqqqq")))
+
+	fmt.Println(bf.Query([]byte("xxx")))
+	fmt.Println(bf.Query([]byte("xxxxxxxxxxxxxxxxxxxxx")))
+
+	bf.Add([]byte("xxxxxxxxxxxxxxxxxxxxx"))
+	fmt.Println(bf.Query([]byte("xxxxxxxxxxxxxxxxxxxxx")))
+}
+
+// Query tests the byte slice input's existence in the BloomFilter structure and returns a boolean value. 
+// The result is either true for existence or false for inexistence. 
+// However it should be noted that false positives are possible, while false negatives are not.
 func (bf *BloomFilter) Query(data []byte) bool {
 	bitLocations := bf.getBitLocations(data)
-	
+
 	for i := 0; i < len(bitLocations); i++ {
 		currLoc := bitLocations[i]
 		sliceLoc := (currLoc - (currLoc % 64)) / 64
-		fmt.Printf("bf.size -> %v bf.bits.len -> %v currLoc -> %v sliceLoc -> %v\n", bf.size, len(bf.bits), currLoc, sliceLoc)
-		if bf.bits[sliceLoc] & (1 << (currLoc % 64)) == 0 {
+		if bf.bits[sliceLoc]&(1<<(currLoc%64)) == 0 {
 			return false
 		}
 	}
 	return true
+}
+
+// Add for thread safe BloomFilterTS structure serves the same purpose as Add for BloomFilter structure.
+// Structure is locked for
+func (bfts *BloomFilterTS) Add(data []byte) {
+	bfts.mtx.Lock()
+	bfts.bf.Add(data)
+	bfts.mtx.Unlock()
+}
+
+// Query for thread safe BloomFilterTS structure serves the same purpose as Query for BloomFilter structure.
+func (bfts *BloomFilterTS) Query(data []byte) bool {
+	bfts.mtx.RLock()
+	defer bfts.mtx.RUnlock()
+	retVal := bfts.bf.Query(data)
+	return retVal
+}
+
+// NewByEstimates requires estimated number of items and estimated false positive rate to create a BloomFilter structure.
+// This function calculates size in bits and ideal number of hash functions that will be created by double hashing of 
+// hash function hash1 and hash function hash2.
+// HashFunction64 hash1 and HashFunction64 hash2 can be nil and when they are nil, a default HashFunction64 for each will be used.
+func NewByEstimates(numItems uint64, fpRate float64, hash1 HashFunction64, hash2 HashFunction64) *BloomFilter {
+	if (numItems == 0) {
+		log.Fatalln("number of items must be positive")
+	}
+	size := uint64(math.Ceil(-1 * float64(numItems) * math.Log(fpRate) / math.Pow(math.Log(2), 2)))
+	numHashFunctions := uint8(math.Ceil(math.Log(2) * float64(size) / float64(numItems)))
+	// fmt.Println(size, numHashFunctions)
+	return NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
+}
+
+func defaultHash1() func(input []byte) uint64 {
+	return func(input []byte) uint64 {
+		h := fnv.New64a()
+		h.Write(input)
+		return h.Sum64()
+	}
+}
+
+func defaultHash2() func(input []byte) uint64 {
+	return func(input []byte) uint64 {
+		h := fnv.New64()
+		h.Write(input)
+		return h.Sum64()
+	}
+}
+
+// func defaultHash2() func(input []byte) uint64 {
+// 	return func(input []byte) uint64 {
+// 		retVal := uint64(0)
+// 		for i := 0; i < len(input); i++ {
+// 			retVal = retVal * 101 + uint64(input[i])
+// 		}
+// 		return retVal
+// 	}
+// }
+
+// NewBySizeAndNumHashFuncs requires maximum size in bits and number of hash functions that will be created via double hashing of
+// hash function hash1 and hash function hash2.
+// HashFunction64 hash1 and HashFunction64 hash2 can be nil and when they are nil, a default HashFunction64 for each will be used.
+// This function returns a new BloomFilter structure.
+func NewBySizeAndNumHashFuncs(size uint64, numHashFunctions uint8, hash1 HashFunction64, hash2 HashFunction64) *BloomFilter {
+	if hash1 == nil {
+		hash1 = defaultHash1()
+	}
+
+	if hash2 == nil {
+		hash2 = defaultHash2()
+	}
+
+	if size == 0 {
+		size = uint64(8 * 1024 * 1024 * 100) // 10 mb in bits
+	}
+
+	l := (size - (size % 64)) / 64
+
+	if size%64 > 0 {
+		l++
+	}
+
+	bits := make([]uint64, l, l)
+
+	if numHashFunctions == uint8(0) {
+		numHashFunctions = uint8(11)
+	}
+
+	bf := BloomFilter{
+		hash1:            hash1,
+		hash2:            hash2,
+		numHashFunctions: numHashFunctions,
+		size:             size,
+		bits:             bits,
+	}
+
+	return &bf
+}
+
+// NewTSByEstimates returns a new BloomFilterTS structure. For more details, please see NewByEstimates function.
+func NewTSByEstimates(numItems uint64, fpRate float64, hash1 HashFunction64, hash2 HashFunction64) *BloomFilterTS {
+	size := uint64(math.Ceil(-1 * float64(numItems) * math.Log(fpRate) / math.Pow(math.Log(2), 2)))
+	numHashFunctions := uint8(math.Ceil(math.Log(2) * float64(size) / float64(numItems)))
+	// fmt.Printf("size %v, numHashFunctions %v", size, numHashFunctions)
+
+	bf := NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
+
+	return &BloomFilterTS{bf: bf}
+}
+
+// NewTSBySizeAndNumHashFuncs returns a new BloomFilterTS structure. For more details, please see NewBySizeAndNumHashFuncs function.
+func NewTSBySizeAndNumHashFuncs(size uint64, numHashFunctions uint8, hash1 HashFunction64, hash2 HashFunction64) *BloomFilterTS {
+	bf := NewBySizeAndNumHashFuncs(size, numHashFunctions, hash1, hash2)
+
+	return &BloomFilterTS{bf: bf}
+}
+
+func (bf *BloomFilter) getBitLocations(data []byte) []uint64 {
+	hash1Val := bf.hash1(data)
+	hash2Val := bf.hash2(data)
+
+	// fmt.Println(string(data), hash1Val, hash2Val)
+
+	retVal := make([]uint64, bf.numHashFunctions)
+
+	for i := uint8(0); i < bf.numHashFunctions; i++ {
+		retVal[i] = (hash1Val + uint64(i)*hash2Val) % (bf.size)
+	}
+	// fmt.Println(retVal)
+	return retVal
 }
